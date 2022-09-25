45071a47ba581c72bc643afa20582b1bd1944935
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2022-09-24 22:45:18 -0300

    Novos exercícios avaliativos

--- exercicios_avaliativos.R
+++ exercicios_avaliativos.R
@@ -80,3 +80,96 @@ dados_municipios%>%
   ) %>%
   arrange(desc(media_populacao_municipios))
 
+
+dados_municipios%>%
+  filter(sigla_uf != "DF") %>%
+  group_by(sigla_uf) %>%
+  summarise(
+    populacao = sum(populacao)
+  ) %>%
+  ungroup() %>%
+  mutate(sigla_uf=reorder(sigla_uf,populacao)) %>%
+  slice_max(order_by = populacao, n=10)%>%
+  ggplot()+
+    geom_col(aes(x=populacao, y=sigla_uf))
+
+dados_municipios %>%
+  slice_max(populacao, n=10) %>%
+  ggplot()+
+  geom_col(aes(x=populacao, y=nome))
+
+dados_municipios %>%
+  slice_max(populacao, n=10) %>%
+  mutate(nome=reorder(nome, populacao)) %>%
+  ggplot()+
+  geom_col(aes(x=populacao, y=nome))
+
+
+dados_municipios %>%
+  slice_head( n=10) %>%
+  mutate(nome=reorder(nome, populacao)) %>%
+  ggplot()+
+  geom_col(aes(x=populacao, y=nome))
+
+
+dados_municipios %>%
+  slice_sample( n=10) %>%
+  mutate(nome=reorder(nome, populacao)) %>%
+  ggplot()+
+  geom_col(aes(x=populacao, y=nome))
+
+library(dados)
+dados_gapminder %>%
+  filter(pais=="Brasil") %>%
+  ggplot()+
+  geom_point(aes(x=pib_per_capita, y=expectativa_de_vida))
+
+
+library(dados)
+dados_gapminder %>%
+  filter(ano==2007) %>%
+  slice_max(order_by = expectativa_de_vida, n=10) %>%
+  mutate(pais = reorder(pais,expectativa_de_vida)) %>%
+  ggplot()+
+  geom_col(aes(x=expectativa_de_vida, y=pais))
+
+library(dados)
+dados_gapminder %>%
+  filter(pais=="Brasil") %>%
+  ggplot()+
+  geom_line(aes(x=ano, y=pib_per_capita))
+
+
+library(dados)
+dados_gapminder %>%
+  filter(pais=="Brasil") %>%
+  ggplot()+
+  geom_line(aes(x=ano, y=expectativa_de_vida))
+
+library(dados)
+dados_gapminder %>%
+  filter(pais=="Argentina") %>%
+  ggplot()+
+  geom_point(aes(x=ano, y=expectativa_de_vida))
+
+library(dados)
+library(tidyverse)
+dados_gapminder %>%
+  mutate(ano = as.character(ano)) %>%
+  ggplot() +
+  geom_boxplot(aes(x=ano, y= expectativa_de_vida)) +
+  scale_y_continuous(breaks=(seq(0,80,20))) +
+  ylim(c(0,NA))
+
+
+
+library(dados)
+library(tidyverse)
+dados_gapminder %>%
+  filter(ano==2007) %>%
+  ggplot() +
+  geom_histogram(aes(x= pib_per_capita), color="white") +
+  scale_y_continuous(breaks = c(seq(0,10,1)))+
+  scale_x_log10()
+
+
